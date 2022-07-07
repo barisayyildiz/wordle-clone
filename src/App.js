c1f1402e0ef6dd5df19ddca1bbcf2f16d55bd4c6
barisayyildiz <37713845+barisayyildiz@users.noreply.github.com>
2022-07-07 20:20:33 +0300

    Bar eklendi

--- src/App.js
+++ src/App.js
@@ -1,42 +1,34 @@
-import { useState } from "react"
-import './App.scss';
+import { useState } from "react";
+import "./App.scss";
 
-import Header from "./Header"
-import Modal from "./Modal"
-import Statistics from "./Statistics"
-import Game from "./Game"
+import Header from "./Header";
+import Modal from "./Modal";
+import Statistics from "./Statistics";
+import Game from "./Game";
+import Bar from "./Bar";
 
 function App() {
-
-	const [isStatisticsModalOpen, setStatisticsModalOpen] = useState(false)
+  const [isStatisticsModalOpen, setStatisticsModalOpen] = useState(true);
 
   return (
     <div className="App">
       <Header
-				title="Wordle Clone"
-				isStatisticsModalOpen={isStatisticsModalOpen}
-				setStatisticsModalOpen={setStatisticsModalOpen}
-			/>
-			<Game
-				setModalStatus={setStatisticsModalOpen}
-			/>
-			{
-				isStatisticsModalOpen && (
-					<Modal
-						title="İSTATİSTİK"
-						width="30%"
-						closeModal={setStatisticsModalOpen}
-						setStatisticsModalOpen={setStatisticsModalOpen}
-					>
-						<Statistics
-							played={0}
-							winPer={0}
-							curStreak={0}
-							maxStreak={0}
-						/>
-					</Modal>
-				)
-			}
+        title="Wordle Clone"
+        isStatisticsModalOpen={isStatisticsModalOpen}
+        setStatisticsModalOpen={setStatisticsModalOpen}
+      />
+      <Game setModalStatus={setStatisticsModalOpen} />
+      {isStatisticsModalOpen && (
+        <Modal
+          title="İSTATİSTİK"
+          width="30%"
+          closeModal={setStatisticsModalOpen}
+          setStatisticsModalOpen={setStatisticsModalOpen}
+        >
+          <Statistics played={0} winPer={0} curStreak={0} maxStreak={0} />
+          <Bar distribution={[1, 0, 0, 0, 0, 0]} />
+        </Modal>
+      )}
     </div>
   );
 }
---
+++ src/Bar/index.js
@@ -0,0 +1,35 @@
+import "./style.scss";
+
+export default function Bar(props) {
+  const { distribution } = props;
+
+  const biggest = Math.max(...distribution);
+
+  const generateWidth = (n) => {
+    return {
+      textAlign: n === 0 ? "center" : "right",
+      width: `${(n / biggest) * 100}%`
+    };
+  };
+
+  return (
+    <div id="guess-distribution">
+      <div className="guess-distribution-header">Tahmin Dağılımı</div>
+      <div className="guess-distribution-content">
+        {[1, 2, 3, 4, 5, 6].map((key) => {
+          return (
+            <div className="guess-distribution-content-row">
+              <div>{key}</div>
+              <div
+                style={generateWidth(distribution[key - 1])}
+                className="guess-bar"
+              >
+                {distribution[key - 1]}
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
